6c0ba50229bd53a176fd3c53f2268fd12ad503cd
ximenacarolina0211 <xime.pelayo0206@gmail.com>
2026-05-09 12:49:19 -0500

    Fix: Rename routes file and update import to resolve routing issues

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import userRoutes from './routes/users.routes.js';
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 
